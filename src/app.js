d2e091f403860f89dfe7bd63bbc7fe82394ecf23
Somnath Paul <08paulsomnath@gmail.com>
2026-05-28 15:41:20 +0530

    define the user schema

--- src/app.js
+++ src/app.js
@@ -3,4 +3,14 @@ import express from 'express';
 // Create an Express application
 const app = express();
 
+
+/**
+ * @route POST /api/auth/register
+ * @desc Register a new user
+ * @access Public
+ */
+app.post('/api/auth/register', (req, res) => {
+    
+});
+
 export default app;
