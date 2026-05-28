113667f8479adcad4878ad3c657c3f9a69c857c7
Somnath Paul <08paulsomnath@gmail.com>
2026-05-28 16:06:52 +0530

    make the create notes api that authenticate the user using the token from the cookie

--- src/app.js
+++ src/app.js
@@ -55,4 +55,52 @@ app.post("/api/auth/register", async (req, res) => {
         .json({ message: "User registered successfully", user: newUser });
 });
 
+/**
+ * @route POST /api/notes
+ * @description Create a new note need title and description in the request body
+ * @access Public
+ */
+app.post("/api/notes", async (req, res) => {
+    const { title, description } = req.body;
+
+    // authenticate the user using the token from the cookie
+    const token = req.cookies.token;
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = user; // { id: "user_id", email: "user_email" }
+
+    // validate the input
+    if (!title) {
+        return res.status(400).json({ error: "Title is required" });
+    }
+
+    if (!description) {
+        return res.status(400).json({ error: "Description is required" });
+    }
+
+    if (title.trim().length < 3) {
+        return res.status(400).json({ error: "Title must be at least 3 characters long" });
+    }
+
+    if (description.trim().length < 10) {
+        return res.status(400).json({ error: "Description must be at least 10 characters long" });
+    }
+
+    // save the note to the database
+
+    const newNote = await NoteModel.create({
+        title,
+        description,
+        user: req.user.email
+    });
+
+    // send the success response with the note data
+    return res.status(201).json({
+        message: "Note created successfully",
+        note: newNote
+    });
+})
+
+
+
 export default app;
